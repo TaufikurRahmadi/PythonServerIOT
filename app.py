1bf67acc09b0f62a1b0f218df53b08aa171f5441
Taufikur Rahmadi <rahmadikhoiron85@gmail.com>
2026-03-09 13:36:18 +0700

    kedua

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ if __name__ == '__main__':
     print("  (Ganti password setelah login pertama!)")
     print("=" * 57)
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
+  #  app.run(host='0.0.0.0', port=5000, debug=True)
